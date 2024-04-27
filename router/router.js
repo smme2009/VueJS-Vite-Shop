6055a1a2d5038b84b21d6df8f0dcc09d3e925c81
smme2009 <smme2009@gmail.com>
2024-04-27 14:49:26 +0800

    feat: 新增管理頁Layout

--- router/router.js
+++ router/router.js
@@ -19,6 +19,10 @@ const router = createRouter({
                         },
                     ],
                 },
+                {
+                    path: 'mgmt',
+                    component: () => import('@/components/layout/Admin.vue'),
+                },
             ],
         },
     ],
---
+++ src/components/layout/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-container>
+        <el-header>Logo</el-header>
+        <el-container>
+            <el-aside width="200px"></el-aside>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<style lang="scss" scoped>
+.el-container {
+    height: 100%
+}
+</style>
