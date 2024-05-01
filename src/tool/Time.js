465beedd5d3135ae2f69673a6e9f25becb993952
smme2009 <smme2009@gmail.com>
2024-05-01 18:58:07 +0800

    feat: 新增時間工具

---
+++ src/tool/Time.js
@@ -0,0 +1,24 @@
+import moment from 'moment-timezone';
+
+const timeZone = 'Asia/Taipei';
+const format = 'YYYY-MM-DD HH:mm:ss';
+
+/**
+ * 取得日期時間
+ * 
+ * @param {int|null} timestamp 時間戳
+ * 
+ * @return {string} 日期時間
+ */
+export function getDateTime(timestamp) {
+    let datetime = '';
+    if (timestamp === null) {
+        datetime = '--';
+    } else {
+        datetime = moment(timestamp * 1000)
+            .tz(timeZone)
+            .format(format);
+    }
+
+    return datetime;
+}
