9f94127206fcabac2f02972f9f6016e79980960b
smme2009 <smme2009@gmail.com>
2024-05-02 21:09:38 +0800

    feat: 設定Element Plus Icon套件

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from '../router/router.js'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
+
+// 導入Icon
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.mount('#app')
