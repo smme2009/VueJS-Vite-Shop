2847b608d23484788e74fb9b9aa0d20cd4c120a6
smme2009 <smme2009@gmail.com>
2024-07-31 16:49:48 +0800

    feat: Vuex增加保持狀態機制

--- src/store/Index.js
+++ src/store/Index.js
@@ -1,8 +1,12 @@
 import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 import page from '@/store/page/Index.js';
 
 const store = createStore({
+    plugins: [
+        createPersistedState()
+    ],
     modules: {
         page: page, // 分頁
     },
