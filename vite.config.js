c7d4c06263b455ccef086dacaa1b70b79ac5aa75
smme2009 <smme2009@gmail.com>
2024-08-16 15:52:43 +0800

    feat: 降低單個chunk的打包大小

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,16 @@ export default defineConfig({
         }
     },
     envDir: path.resolve(__dirname),
+    // 降低單個chunk的打包大小
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 })
