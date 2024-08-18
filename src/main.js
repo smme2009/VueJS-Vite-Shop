78da867de8a42430e42317cc15d9e3d6158ff45e
smme2009 <smme2009@gmail.com>
2024-08-18 20:15:03 +0800

    feat: 新增Pinia套件

--- src/main.js
+++ src/main.js
@@ -6,12 +6,19 @@ import 'element-plus/dist/index.css';
 import router from '@/router/Index.js';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import store from '@/store/Index.js';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App);
 
 app.use(ElementPlus); // 導入Element Plus
 app.use(router); // 導入Vue Router
 app.use(store); // 導入Vuex
+
+// 導入Pinia
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate) // 記憶狀態套件
+app.use(pinia);
 
 // 導入Icon
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
