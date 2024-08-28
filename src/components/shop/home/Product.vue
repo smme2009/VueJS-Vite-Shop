7b0702225d1accdae4368885decaa3e729200e8f
smme2009 <smme2009@gmail.com>
2024-08-28 19:12:40 +0800

    feat: 調整商店的商品卡片樣式

--- src/components/shop/home/Product.vue
+++ src/components/shop/home/Product.vue
@@ -4,27 +4,39 @@
             v-for="product in store.data"
             class="xs:w-1/2 sm:w-1/3 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 p-1"
         >
-            <el-card class="!rounded-lg">
-                <div class="flex justify-center">
+            <el-card
+                class="h-full !rounded-lg"
+                body-class="h-full flex flex-col p-0"
+            >
+                <div class="flex-auto">
                     <el-image
-                        class="xs:h-40 ms:h-48 xs:w-40 ms:w-48 rounded-lg"
+                        class="h-full"
                         :src="product.photoUrl"
+                        fit="fill"
+                        :title="product.name"
                     />
                 </div>
-                <p class="truncate">{{ product.name }}</p>
-                <p class="text-red-600 font-bold">${{ product.price }}</p>
-                <template #footer>
-                    <div class="flex justify-end">
-                        <el-button-group>
-                            <el-tooltip content="直接購買">
-                                <el-button icon="Goods" />
-                            </el-tooltip>
-                            <el-tooltip content="加入購物車">
-                                <el-button icon="ShoppingCart" />
-                            </el-tooltip>
-                        </el-button-group>
-                    </div>
-                </template>
+                <div class="flex justify-between p-2">
+                    <p class="truncate" :title="product.name">
+                        {{ product.name }}
+                    </p>
+                    <p
+                        class="text-red-600 font-bold"
+                        :title="'$' + product.price"
+                    >
+                        ${{ product.price }}
+                    </p>
+                </div>
+                <div class="flex justify-end p-2">
+                    <el-button-group>
+                        <el-tooltip content="直接購買">
+                            <el-button icon="Goods" />
+                        </el-tooltip>
+                        <el-tooltip content="加入購物車">
+                            <el-button icon="ShoppingCart" />
+                        </el-tooltip>
+                    </el-button-group>
+                </div>
             </el-card>
         </div>
     </div>
