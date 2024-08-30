9cb4544d35c1b8f94bc94a43cd2a15552fee8ed2
smme2009 <smme2009@gmail.com>
2024-08-30 17:13:42 +0800

    fix: 修正商店的卷軸失效問題

--- src/components/shop/layout/shop/Index.vue
+++ src/components/shop/layout/shop/Index.vue
@@ -1,13 +1,13 @@
 <template>
-    <el-container class="h-full flex justify-center">
-        <div class="xs:w-full sm:w-[90%] md:w-[80%] xl:w-[70%] 2xl:w-[60%]">
-            <el-header>
-                <Header />
-            </el-header>
-            <el-main>
+    <el-container class="h-full">
+        <el-header>
+            <Header />
+        </el-header>
+        <el-main class="h-full flex justify-center">
+            <div class="xs:w-full sm:w-[90%] md:w-[80%] xl:w-[70%] 2xl:w-[60%]">
                 <router-view></router-view>
-            </el-main>
-        </div>
+            </div>
+        </el-main>
     </el-container>
 </template>
 
