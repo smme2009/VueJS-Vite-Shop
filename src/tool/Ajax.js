981baec0950cd1feb17563dd4eb87f5f711c77a3
smme2009 <smme2009@gmail.com>
2024-09-29 22:30:03 +0800

    feat: 優化Ajax工具

--- src/tool/Ajax.js
+++ src/tool/Ajax.js
@@ -14,26 +14,89 @@ import toolNotify from "@/tool/Notify.js";
  * @param {object} param.data 資料
  * @param {object} param.header Header
  * 
- * @returns {object}
+ * @returns {object} 回傳結果
  */
 const ajax = async (param) => {
-    const apiUrl = import.meta.env.VITE_API_URL;
-    const fullUrl = apiUrl + param.uri;
-    const jwtToken = getJwtToken(param.apiTarget);
+    // 取得Axios設定
+    const setting = getSetting({
+        method: param.method,
+        url: import.meta.env.VITE_API_URL + param.uri,
+        header: param.header ?? {},
+        jwtToken: getJwtToken(param.apiTarget),
+        data: param.data ?? {},
+    });
 
-    // 基本設定
+    // 發送Ajax
+    const response = await sendAjax(setting, param.apiTarget);
+
+    return response;
+}
+
+export default ajax;
+
+/**
+ * 取得Axios設定
+ * 
+ * @param {object} param 參數
+ * @param {string} param.method 方法
+ * @param {string} param.url 網址
+ * @param {object} param.header Header
+ * @param {string} param.jwtToken JWT Token
+ * @param {object} param.data 資料
+ * 
+ * @returns {object} 設定 
+ */
+const getSetting = (param) => {
+    const paramName = (param.method === "get") ? "params" : "data";
+
     const setting = {
         method: param.method,
-        url: fullUrl,
+        url: param.url,
         headers: {
-            Authorization: "Bearer " + jwtToken,
-            ...(param.header ?? {}),
+            ...param.header,
+            Authorization: "Bearer " + param.jwtToken,
         },
+        [paramName]: param.data,
     };
 
-    const paramName = (param.method === "get") ? "params" : "data";
-    setting[paramName] = param.data ?? {};
+    return setting;
+};
 
+/**
+ * 取得JWT Token
+ * 
+ * @param {string} apiTarget API目標
+ * 
+ * @returns {string} JWT Token
+ */
+const getJwtToken = (apiTarget) => {
+    let jwtToken = "";
+    switch (apiTarget) {
+        case 'mgmt':
+            const storeAdmin = new storeBeAdmin();
+            jwtToken += storeAdmin.jwtToken;
+            break;
+        case 'shop':
+            const storeMember = new storeBeMember();
+            jwtToken += storeMember.jwtToken;
+            break;
+        case 'public':
+            // 不需JWT Token，所以暫無動作
+            break;
+    }
+
+    return jwtToken;
+};
+
+/**
+ * 發送Ajax
+ * 
+ * @param {object} setting 設定
+ * @param {string} apiTarget API目標
+ * 
+ * @returns {object} 回傳結果
+ */
+const sendAjax = async (setting, apiTarget) => {
     let response = {};
     await axios(setting)
         .then((r) => {
@@ -42,11 +105,12 @@ const ajax = async (param) => {
                 message: r.data.message,
                 data: r.data.data,
             };
-        }).catch((e) => {
+        })
+        .catch((e) => {
             switch (e.response.status) {
                 case 401: // 無權限時強制跳轉登入頁
                     toolNotify("error", "請重新登入");
-                    router.push({ name: `${param.apiTarget}Login` });
+                    router.push({ name: `${apiTarget}Login` });
                     break;
                 case 500: // 系統錯誤統一處理
                     toolNotify("error", "系統異常");
@@ -70,31 +134,3 @@ const ajax = async (param) => {
 
     return response;
 }
-
-/**
- * 取得JWT Token
- * 
- * @param {string} apiTarget API 目標
- * 
- * @returns {string} 
- */
-const getJwtToken = (apiTarget) => {
-    let jwtToken = '';
-    switch (apiTarget) {
-        case 'mgmt':
-            const storeAdmin = new storeBeAdmin();
-            jwtToken = storeAdmin.jwtToken;
-            break;
-        case 'shop':
-            const storeMember = new storeBeMember();
-            jwtToken = storeMember.jwtToken;
-            break;
-        case 'public':
-            // 不需JWT Token，所以暫無動作
-            break;
-    }
-
-    return jwtToken;
-};
-
-export default ajax;
