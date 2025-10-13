8cde6f4b3a8d74e669cdaeca91d80958e803a4f8
smme2009 <smme2009@gmail.com>
2025-10-13 22:30:20 +0800

    feat: 導入Element Plus套件

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
+// 建立Vue的實例
 const app = createApp(App)
 
+// 引入套件
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
+// 掛載Vue實例
 app.mount('#app')
