8f3bd5f96873f744e50ccb3f7d701afe6f7c9db2
smme2009 <smme2009@gmail.com>
2025-11-19 22:14:30 +0800

    feat: 對話框組件加入換行機制

--- src/components/dialog/dialog.vue
+++ src/components/dialog/dialog.vue
@@ -1,6 +1,10 @@
 <template>
-  <el-dialog class="max-w-md" v-model="storeDialog.visible" title="訊息">
-    <span>{{ storeDialog.message }}</span>
+  <el-dialog
+    class="max-w-md"
+    v-model="storeDialog.visible"
+    title="訊息"
+  >
+    <span class="whitespace-pre-line">{{ storeDialog.message }}</span>
     <template #footer>
       <div class="dialog-footer">
         <el-button type="primary" @click="close">確定</el-button>
