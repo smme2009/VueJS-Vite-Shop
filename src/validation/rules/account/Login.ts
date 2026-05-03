b3747ceaf4aaaf6df268557c9934b752fd35ea4a
smme2009 <smme2009@gmail.com>
2026-05-03 10:13:14 +0800

    feat: 調整管理員登入的欄位驗證規則

--- src/validation/rules/account/Login.ts
+++ src/validation/rules/account/Login.ts
@@ -4,8 +4,16 @@ import * as yup from 'yup'
 // 表單驗證-帳號-登入
 export default yup.object<ILogin>({
   // 帳號
-  account: yup.string().required('請填寫此欄位'),
+  account: yup
+    .string()
+    .required('請填寫此欄位')
+    .min(8, '最少需要輸入8個字')
+    .max(12, '最多只能輸入12個字'),
 
   // 密碼
-  password: yup.string().required('請填寫此欄位'),
+  password: yup
+    .string()
+    .required('請填寫此欄位')
+    .min(8, '最少需要輸入8個字')
+    .max(12, '最多只能輸入12個字'),
 })
