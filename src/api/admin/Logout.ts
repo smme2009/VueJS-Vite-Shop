92affd4b00955d23d238dc7a495778fede2beba4
smme2009 <smme2009@gmail.com>
2026-07-14 22:54:00 +0800

    feat: 明確化登出API回傳資料的型別

--- src/api/admin/Logout.ts
+++ src/api/admin/Logout.ts
@@ -8,9 +8,9 @@ export default class Logout extends Api {
   /**
    * 登出
    *
-   * @returns {Promise<null | IResponse<null>>} 登出資料
+   * @returns {Promise<null | IResponse<[]>>} 登出資料
    */
-  public async logout(): Promise<null | IResponse<null>> {
+  public async logout(): Promise<null | IResponse<[]>> {
     return await this.toolAjax().post('/mgmt/logout')
   }
 }
